315ad34dfd14e753872403cd624b7b1ce285379c
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2022-06-09 15:31:20 +0100

    Implemented Balance section

--- screens/Home.js
+++ screens/Home.js
@@ -56,9 +56,21 @@ const Home = ({ navigation }) => {
                             />
                         </TouchableOpacity>
                     </View>
+
                     {/* Balance */}
+                    <View
+                        style={{
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Text style={{ color: COLORS.white, ...FONTS.h3 }}>Your Portfolio Balance</Text>
+                        <Text style={{ marginTop: SIZES.base, color: COLORS.white, ...FONTS.h1 }}>${dummyData.portfolio.balance}</Text>
+                        <Text style={{ color: COLORS.white, ...FONTS.body5 }}>{dummyData.portfolio.changes} Last 24 hours</Text>
+                    </View>
 
                     {/* Trending */}
+                    
                 </ImageBackground>
             </View>
         )
