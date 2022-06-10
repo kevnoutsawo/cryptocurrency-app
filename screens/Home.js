9e0ad8217f2344b92b1d0cbe344663c436f74ce2
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2022-06-10 12:39:01 +0100

    Implemented Trending currencies section

--- screens/Home.js
+++ screens/Home.js
@@ -14,7 +14,48 @@ import { dummyData, COLORS, SIZES, FONTS, icons, images } from '../constants';
 
 const Home = ({ navigation }) => {
 
+    const [trending, setTrending] = React.useState(dummyData.trendingCurrencies)
+
     function renderHeader() {
+
+        const renderItem = ({item, index}) => (
+            <TouchableOpacity
+                style={{
+                    width: 180,
+                    paddingVertical: SIZES.padding,
+                    paddingHorizontal: SIZES.padding,
+                    marginLeft: index == 0 ? SIZES.padding : SIZES.base,
+                    borderRadius: 10,
+                    backgroundColor: COLORS.white
+                }}
+            >
+                {/* Currency */}
+                <View style={{flexDirection: 'row'}}>
+                    <View>
+                        <Image
+                            source={item.image}
+                            resizeMode="cover"
+                            style={{
+                                marginTop: 5,
+                                width: 25,
+                                height: 25
+                            }}
+                        />
+                    </View>
+                    <View style={{ marginLeft: SIZES.base }}>
+                        <Text style={{ ...FONTS.h2 }}>{item.currency}</Text>
+                        <Text style={{ color: COLORS.gray, ...FONTS.body3 }}>{item.code}</Text>
+                    </View>
+                </View>
+
+                {/* Value */}
+                <View style={{ marginTop: SIZES.radius }}>
+                    <Text style={{ ...FONTS.h2 }}>${item.amount}</Text>
+                    <Text style={{ color: item.type == "I" ? COLORS.green : COLORS.red, ...FONTS.h3 }}>{item.changes}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+
         return (
             <View
                 style={{
@@ -70,7 +111,23 @@ const Home = ({ navigation }) => {
                     </View>
 
                     {/* Trending */}
-                    
+                    <View
+                        style={{
+                            position: 'absolute',
+                            bottom: "-30%"
+                        }}
+                    >
+                        <Text style={{ marginLeft: SIZES.padding,
+                         color: COLORS.white, ...FONTS.h2 }}>Trending</Text>
+                         <FlatList
+                            contentContainerStyle={{marginTop: SIZES.base}}
+                            data={trending}
+                            renderItem={renderItem}
+                            keyExtractor={item => `${item.id}`}
+                            horizontal
+                            showsHorizontalScrollIndicator={false}
+                         />
+                    </View>
                 </ImageBackground>
             </View>
         )
